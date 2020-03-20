cda995f252bc97cc07b971340365096892b93b8c
estsn122 <y.tani9411@gmail.com>
2020-03-20 15:20:28 +0900

    Fix:registered_on属性の役割がcreated_atで代用できるため削除

--- app/models/imported_user.rb
+++ app/models/imported_user.rb
@@ -6,9 +6,7 @@ class ImportedUser < ApplicationRecord
   def self.import_users
     twitter = twitter_rest_client
     twitter.search(Settings.get_user_param.word, count: Settings.get_user_param.num).take(Settings.get_user_param.num).each do |tweet|
-      ImportedUser.find_or_create_by(twitter_id: tweet.user.id.to_s) do |user|
-        user.registered_on = Date.today
-      end
+      ImportedUser.find_or_create_by(twitter_id: tweet.user.id.to_s)
     end
   end
 end
--- db/migrate/20200316095949_create_imported_users.rb
+++ db/migrate/20200316095949_create_imported_users.rb
@@ -2,7 +2,6 @@ class CreateImportedUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :imported_users do |t|
       t.string :twitter_id, null: false, index: { unique: true }
-      t.date :registered_on, null: false
       t.timestamps
     end
   end
