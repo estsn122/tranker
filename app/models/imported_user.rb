dc95208bcfb0728b662fcc6256a4a6fd88a35897
estsn122 <y.tani9411@gmail.com>
2020-03-20 15:55:19 +0900

    Refactor:微調整

--- app/models/imported_user.rb
+++ app/models/imported_user.rb
@@ -2,8 +2,7 @@ class ImportedUser < ApplicationRecord
   include TwitterRestClient
 
   def self.import_users
-    twitter = twitter_rest_client
-    twitter.search(Settings.get_user_param.word, count: Settings.get_user_param.num).take(Settings.get_user_param.num).each do |tweet|
+    twitter_rest_client.search(Settings.get_user_param.word, count: Settings.get_user_param.num).take(Settings.get_user_param.num).each do |tweet|
       ImportedUser.find_or_create_by(twitter_id: tweet.user.id.to_s)
     end
   end
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,5 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    # TODO ウィンドウが立ち上がらないので対処
     # driven_by(:rack_test)
     driven_by(:selenium_chrome)
   end
