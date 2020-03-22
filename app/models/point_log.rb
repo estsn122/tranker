f198c820b38a11f76f0c3d93e342fb30e137ae6e
estsn122 <y.tani9411@gmail.com>
2020-03-22 18:21:37 +0900

    Fix:変数名を微調整

--- app/models/point_log.rb
+++ app/models/point_log.rb
@@ -6,20 +6,20 @@ class PointLog < ApplicationRecord
   validates :twitter_id, uniqueness: { scope: :aggregated_on }
 
   def self.aggregate_follow_user
-    imported_user_id = ImportedUser.where(registered_on: nil).first.twitter_id
-    return unless imported_user_id
+    aggregate_id = ImportedUser.where(registered_on: nil).first.twitter_id
+    return unless aggregate_id
 
-    target_ids = PointLog.target_following_ids(imported_user_id)
+    target_ids = PointLog.target_following_ids(aggregate_id)
     return unless target_ids
 
     PointLog.calculate_result(target_ids)
     FollowedUser.import_followed_users(target_ids)
-    ImportedUser.find_by(twitter_id: imported_user_id).update(registered_on: Date.today)
+    ImportedUser.find_by(twitter_id: aggregate_id).update(registered_on: Date.today)
   end
 
-  def self.target_following_ids(imported_user_id)
+  def self.target_following_ids(confirm_user_id)
     # NOTE: 取得するユーザー数を絞っているのは負荷を減らすため。取得IDは小さい順や辞書順ではないため、それにより結果が偏ることはない。
-    following_ids = twitter_rest_client.friend_ids(imported_user_id.to_i).attrs[:ids].first(Settings.aggregate_param.get_follow_user_num)
+    following_ids = twitter_rest_client.friend_ids(confirm_user_id.to_i).attrs[:ids].first(Settings.aggregate_param.get_follow_user_num)
     truncation_ids = FollowedUser.where(truncation: true).map { |n| n.twitter_id.to_i }
     following_ids - truncation_ids
   end
