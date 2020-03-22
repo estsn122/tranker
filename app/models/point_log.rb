a569e1c2ba65bed4c1dedb55a4140c00242a16df
estsn122 <y.tani9411@gmail.com>
2020-03-22 11:29:34 +0900

    Add:フォロワー数で足切りする機能を実装(案1)

--- app/models/point_log.rb
+++ app/models/point_log.rb
@@ -9,11 +9,27 @@ class PointLog < ApplicationRecord
     twitter_id = ImportedUser.where(registered_on: nil).first.twitter_id
     return unless twitter_id
 
-    logs = []
-    twitter_rest_client.friend_ids(twitter_id.to_i).attrs[:ids].first(Settings.aggregate_param.get_follow_user_num).each do |following_id|
-      logs << PointLog.new(twitter_id: following_id, points: 1, aggregated_on: Date.today)
+    # NOTE: 取得するユーザー数を絞っているのは負荷を減らすため。取得IDは小さい順や辞書順ではないため、結果が偏ることはない。
+    hairetu = twitter_rest_client.friend_ids(twitter_id.to_i).attrs[:ids].first(Settings.aggregate_param.get_follow_user_num)
+    tw = PointLog.truncate_by_follower_num(hairetu)
+
+    if tw
+      logs = []
+      tw.each do |following|
+        logs << PointLog.new(twitter_id: following.id.to_s, points: 1, aggregated_on: Date.today)
+      end
+      PointLog.import logs, on_duplicate_key_update: 'points = points + 1'
     end
-    PointLog.import logs, on_duplicate_key_update: 'points = points + 1'
     ImportedUser.find_by(twitter_id: twitter_id).update(registered_on: Date.today)
+  end
+
+  def self.truncate_by_follower_num(twitter_ids)
+    twitter = twitter_rest_client
+    list = []
+    twitter_ids.each_slice(100).to_a.each do |follow_array|
+      list << twitter.users(follow_array).select { |follow| follow.followers_count > 1000 }
+    end
+    list.delete(nil)
+    list.flatten!
   end
 end
