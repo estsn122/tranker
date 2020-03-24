6d4ec04d1861aebc6df7443ca1ea49ab8827b358
estsn122 <y.tani9411@gmail.com>
2020-03-24 20:57:36 +0900

    Fix:rubocopの指摘を修正

--- app/models/concerns/twitter_rest_client.rb
+++ app/models/concerns/twitter_rest_client.rb
@@ -3,7 +3,7 @@ module TwitterRestClient
 
   class_methods do
     def twitter_rest_client
-      @client ||= Twitter::REST::Client.new do |config|
+      @twitter_rest_client ||= Twitter::REST::Client.new do |config|
         config.consumer_key        = Settings.twitter.consumer_key
         config.consumer_secret     = Settings.twitter.consumer_secret
         config.access_token        = Settings.twitter.access_token
