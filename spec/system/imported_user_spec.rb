2e2b74a59cf6b09873044d6f4df5e509fe43cff1
estsn122 <y.tani9411@gmail.com>
2020-03-24 20:26:13 +0900

    Add:RspecでTwitterAPIを実行する際、vcrに対応

--- spec/system/imported_user_spec.rb
+++ spec/system/imported_user_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'ImportedUsers', type: :system do
   describe 'ユーザーIDの取得機能' do
     let(:imported_user) { ImportedUser.import_users }
-    it 'ツイートが取得できる' do
+    it 'ツイートが取得できる', vcr: true do
       expect(imported_user.first.to_s).to include '#<Twitter::Tweet:'
     end
-    it '取得するツイート数が正しい' do
+    it '取得するツイート数が正しい', vcr: true  do
       expect(imported_user.length).to eq Settings.get_user_param.num
     end
   end
