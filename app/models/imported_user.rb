37b2e1ebec49a6751249fbf3486fb1ba298928e3
estsn122 <y.tani9411@gmail.com>
2020-03-25 22:27:27 +0900

    Fix:微調整

--- app/models/imported_user.rb
+++ app/models/imported_user.rb
@@ -3,10 +3,10 @@ class ImportedUser < ApplicationRecord
   validates :twitter_id, presence: true, uniqueness: true
 
   def self.import_users
-    import_users = []
+    users = []
     twitter_rest_client.search(Settings.get_user_param.word, count: Settings.get_user_param.num).take(Settings.get_user_param.num).each do |tweet|
-      import_users << ImportedUser.new(twitter_id: tweet.user.id.to_s)
+      users << ImportedUser.new(twitter_id: tweet.user.id.to_s)
     end
-    ImportedUser.import import_users, on_duplicate_key_ignore: true
+    ImportedUser.import users, on_duplicate_key_ignore: true
   end
 end
