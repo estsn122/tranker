70d888ccb4d388664d459779ef485472dae7b516
estsn122 <y.tani9411@gmail.com>
2020-03-25 19:36:37 +0900

    Fix:imported_userをバルクインサートで追加するよう修正

--- app/models/imported_user.rb
+++ app/models/imported_user.rb
@@ -3,8 +3,10 @@ class ImportedUser < ApplicationRecord
   validates :twitter_id, presence: true, uniqueness: true
 
   def self.import_users
+    import_users = []
     twitter_rest_client.search(Settings.get_user_param.word, count: Settings.get_user_param.num).take(Settings.get_user_param.num).each do |tweet|
-      ImportedUser.find_or_create_by(twitter_id: tweet.user.id.to_s)
+      import_users << ImportedUser.new(twitter_id: tweet.user.id.to_s)
     end
+    ImportedUser.import import_users, on_duplicate_key_ignore: true
   end
 end
