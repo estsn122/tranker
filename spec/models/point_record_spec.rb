4d57eed2acf64f56a9f6717ba60a358bb59bcc5c
estsn122 <y.tani9411@gmail.com>
2020-03-28 19:39:50 +0900

    Fix:モデルスペックの変数名を修正

--- spec/models/point_record_spec.rb
+++ spec/models/point_record_spec.rb
@@ -2,34 +2,34 @@ require 'rails_helper'
 
 describe 'PointRecord', type: :model do
   it 'twitter_idがないと無効' do
-    point_log = build(:point_record, twitter_id: '')
-    point_log.valid?
-    expect(point_log.errors[:twitter_id]).to include('を入力してください')
+    point_record = build(:point_record, twitter_id: '')
+    point_record.valid?
+    expect(point_record.errors[:twitter_id]).to include('を入力してください')
   end
   it 'pointsがないと無効' do
-    point_log = build(:point_record, points: '')
-    point_log.valid?
-    expect(point_log.errors[:points]).to include('を入力してください')
+    point_record = build(:point_record, points: '')
+    point_record.valid?
+    expect(point_record.errors[:points]).to include('を入力してください')
   end
   it 'recorded_onがないと無効' do
-    point_log = build(:point_record, recorded_on: '')
-    point_log.valid?
-    expect(point_log.errors[:recorded_on]).to include('を入力してください')
+    point_record = build(:point_record, recorded_on: '')
+    point_record.valid?
+    expect(point_record.errors[:recorded_on]).to include('を入力してください')
   end
   it 'twitter_idは重複しても有効(recorded_onは異なる)' do
     create(:point_record, twitter_id: '654321', recorded_on: Date.yesterday)
-    point_log = build(:point_record, twitter_id: '654321', recorded_on: Date.today)
-    expect(point_log.valid?).to be true
+    point_record = build(:point_record, twitter_id: '654321', recorded_on: Date.today)
+    expect(point_record.valid?).to be true
   end
   it 'recorded_onは重複しても有効(twitter_idは異なる)' do
     create(:point_record, twitter_id: '54321',recorded_on: Date.today)
-    point_log = build(:point_record, twitter_id: '51234', recorded_on: Date.today)
-    expect(point_log.valid?).to be true
+    point_record = build(:point_record, twitter_id: '51234', recorded_on: Date.today)
+    expect(point_record.valid?).to be true
   end
   it 'twitter_idとrecorded_onの組み合わせは重複すると無効' do
     create(:point_record, twitter_id: '76543', recorded_on: Date.today)
-    point_log = build(:point_record, twitter_id: '76543', recorded_on: Date.today)
-    point_log.valid?
-    expect(point_log.errors[:twitter_id]).to include('はすでに存在します')
+    point_record = build(:point_record, twitter_id: '76543', recorded_on: Date.today)
+    point_record.valid?
+    expect(point_record.errors[:twitter_id]).to include('はすでに存在します')
   end
 end
