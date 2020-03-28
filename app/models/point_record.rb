d9eacfd6ee97d1f2d7c17db780f9b01792ae8f23
estsn122 <y.tani9411@gmail.com>
2020-03-28 20:16:39 +0900

    Fix:例外処理を考慮したコードに修正

--- app/models/point_record.rb
+++ app/models/point_record.rb
@@ -7,14 +7,14 @@ class PointRecord < ApplicationRecord
   belongs_to :followed_user, foreign_key: 'twitter_id', primary_key: 'twitter_id', optional: true
 
   def self.aggregate_follow_users
-    twitter_id = ImportedUser.where(aggregate_following_users_on: nil).first.twitter_id
-    return unless twitter_id
+    imported_user = ImportedUser.where(aggregate_following_users_on: nil).first
+    return unless imported_user
 
     new_point_records = []
-    twitter_rest_client.friend_ids(twitter_id.to_i).attrs[:ids].first(Settings.aggregate_param.get_follow_user_num).each do |following_id|
+    twitter_rest_client.friend_ids(imported_user.twitter_id.to_i).attrs[:ids].first(Settings.aggregate_param.get_follow_user_num).each do |following_id|
       new_point_records << PointRecord.new(twitter_id: following_id, points: 1, recorded_on: Date.today)
     end
-    PointRecord.import new_point_records, on_duplicate_key_update: 'points = points + 1'
-    ImportedUser.find_by(twitter_id: twitter_id).update(aggregate_following_users_on: Date.today)
+    PointRecord.import! new_point_records, on_duplicate_key_update: 'points = points + 1'
+    ImportedUser.find_by!(twitter_id: imported_user.twitter_id).update(aggregate_following_users_on: Date.today)
   end
 end
