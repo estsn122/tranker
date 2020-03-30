93bab6e38d82009bdd4dc9fa494dc40d8dc347f1
estsn122 <y.tani9411@gmail.com>
2020-03-30 23:23:16 +0900

    Refactor:rankersを取得する際、IN句を使用するよう変更

--- app/controllers/followed_users_controller.rb
+++ app/controllers/followed_users_controller.rb
@@ -1,10 +1,6 @@
 class FollowedUsersController < ApplicationController
   def index
-    @rankers = []
     ids = FollowedUser.joins(:point_records).group(:twitter_id).sum(:points).sort_by { |_, v| -v }.first(FollowedUser::LOWEST_RANK_TO_DISPLAY)
-    ids.each do |id, _|
-      @rankers << FollowedUser.find_by(twitter_id: id)
-    end
-    @rankers
+    @rankers = FollowedUser.where(twitter_id: ids.map { |id, _| id.to_i })
   end
 end
