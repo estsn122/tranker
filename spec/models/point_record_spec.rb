19c7323d3efe2b3a3b89b169efb4703cf2eb4d6f
estsn122 <y.tani9411@gmail.com>
2020-03-30 13:25:52 +0900

    Fix:modelスペックの不備を修正

--- spec/models/point_record_spec.rb
+++ spec/models/point_record_spec.rb
@@ -17,16 +17,20 @@ describe 'PointRecord', type: :model do
     expect(point_record.errors[:recorded_on]).to include('を入力してください')
   end
   it 'twitter_idは重複しても有効(recorded_onは異なる)' do
+    create(:followed_user, twitter_id:'654321')
     create(:point_record, twitter_id: '654321', recorded_on: Date.yesterday)
     point_record = build(:point_record, twitter_id: '654321', recorded_on: Date.today)
     expect(point_record).to be_valid
   end
   it 'recorded_onは重複しても有効(twitter_idは異なる)' do
+    create(:followed_user, twitter_id:'54321')
+    create(:followed_user, twitter_id:'51234')
     create(:point_record, twitter_id: '54321',recorded_on: Date.today)
     point_record = build(:point_record, twitter_id: '51234', recorded_on: Date.today)
     expect(point_record).to be_valid
   end
   it 'twitter_idとrecorded_onの組み合わせは重複すると無効' do
+    create(:followed_user, twitter_id:'76543')
     create(:point_record, twitter_id: '76543', recorded_on: Date.today)
     point_record = build(:point_record, twitter_id: '76543', recorded_on: Date.today)
     point_record.valid?
