de00644620748895821126c09842d4647127d0c8
estsn122 <y.tani9411@gmail.com>
2020-03-31 18:43:39 +0900

    Fix:問い合わせメソッドを使い、メソッドの長大化を抑制

--- app/models/followed_user.rb
+++ app/models/followed_user.rb
@@ -9,8 +9,12 @@ class FollowedUser < ApplicationRecord
     ranker_ids = FollowedUser.joins(:point_records).group(:twitter_id).sum(:points).sort_by { |_, v| -v }.first(LOWEST_RANK_TO_DISPLAY).map { |n, m| [n.to_i, m] }
     return unless ranker_ids
 
-    FollowedUser.update_all(total_points: 0)
     update_keys = %i[twitter_id followers_num total_points name screen_name profile profile_image_url]
+    FollowedUser.import! update_keys, update_values(ranker_ids), on_duplicate_key_update: update_keys - [:twitter_id]
+  end
+
+  def self.update_values(ranker_ids)
+    FollowedUser.update_all(total_points: 0)
     update_values = []
     ranker_ids.each_slice(100) do |hundred_in_total_ranker|
       rankers_points = hundred_in_total_ranker.map { |i| i[1] }
@@ -18,7 +22,7 @@ class FollowedUser < ApplicationRecord
         update_values << [ranker.id.to_s, ranker.followers_count, rankers_points.shift, ranker.name.to_s, ranker.screen_name, ranker.description.to_s, ranker.profile_image_url_https.to_s]
       end
     end
-    FollowedUser.import! update_keys, update_values, on_duplicate_key_update: %i[followers_num total_points name screen_name profile profile_image_url]
+    update_values
   end
 
   def followed_button_url(screen_name)
